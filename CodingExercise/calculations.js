6da6e2dd98236659d920a1ac7697d93369ee7213
maxoldshein <oldshein@bc.edu>
2017-05-15 05:03:03 -0400

    Updated HTML/CSS for Athenium Challenge.

--- CodingExercise/calculations.js
+++ CodingExercise/calculations.js
@@ -15,7 +15,7 @@ function calculateHours() {
 	if(employeeName == "" || employeeName == null || hourlyWage == "" || hourlyWage == null || weekDates == "" || weekDates == null || mondayHours == "" || mondayHours == null || tuesdayHours == "" || tuesdayHours == null || wednesdayHours == "" || wednesdayHours == null || thursdayHours === "" || thursdayHours == null || fridayhours === "" || fridayHours == null) {
 		window.alert("Make sure that all of the fields are filled in before you click 'Calculate'!");
 	} else {
-		var hourlyWageNumber = parseInt(hourlyWage);
+		var hourlyWageNumber = parseFloat(hourlyWage);
 
 		var text = "Hourly wage is: " + hourlyWageNumber;
 
