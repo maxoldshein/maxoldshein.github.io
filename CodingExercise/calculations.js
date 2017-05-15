6b9d18d90bcab3821f694d332210b5f81f8843f0
maxoldshein <oldshein@bc.edu>
2017-05-15 10:09:19 -0400

    Updated HTML/CSS/JavaScript for Athenium Challenge; added footer.

--- CodingExercise/calculations.js
+++ CodingExercise/calculations.js
@@ -87,7 +87,7 @@ function checkHourlyWageCookie() {
 	return wage;
 }
 
-function clearCookies() {
+function clearNameWageCookies() {
 	document.cookie="employeeName=; expires=Mon, 01 Jan 1990 00:00:00 UTC; path=/;";
 	document.cookie="hourlyWage=; expires=Mon, 01 Jan 1990 00:00:00 UTC, path=/;";
 	return false;
