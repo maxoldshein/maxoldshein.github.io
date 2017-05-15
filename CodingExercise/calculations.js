9c09102e0982df6db51506b442224cbba49485c3
maxoldshein <oldshein@bc.edu>
2017-05-15 05:01:44 -0400

    Updated HTML/CSS for Athenium Challenge.

--- CodingExercise/calculations.js
+++ CodingExercise/calculations.js
@@ -15,10 +15,9 @@ function calculateHours() {
 	if(employeeName == "" || employeeName == null || hourlyWage == "" || hourlyWage == null || weekDates == "" || weekDates == null || mondayHours == "" || mondayHours == null || tuesdayHours == "" || tuesdayHours == null || wednesdayHours == "" || wednesdayHours == null || thursdayHours === "" || thursdayHours == null || fridayhours === "" || fridayHours == null) {
 		window.alert("Make sure that all of the fields are filled in before you click 'Calculate'!");
 	} else {
-		var totalHours = parseInt(mondayHours) + parseInt(tuesdayHours) + parseInt(wednesdayhours) + parseInt(thursdayHours) + parseInt(fridayHours);
-		var totalPayment = parseFloat(hourlyWage) * totalHours;
+		var hourlyWageNumber = parseInt(hourlyWage);
 
-		var text = "Payment for " + employeeName + " for the week " + weekDates + ": " + totalPayment;
+		var text = "Hourly wage is: " + hourlyWageNumber;
 
 		var divobject = document.getElementById('hourCalculations');
 		divobject.innerHTML = text;
