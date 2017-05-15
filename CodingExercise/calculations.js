898b60d5ecbafcd54708b891c204ecc32ee8b7b4
maxoldshein <oldshein@bc.edu>
2017-05-15 04:24:42 -0400

    Updated HTML/CSS for Athenium Challenge.

--- CodingExercise/calculations.js
+++ CodingExercise/calculations.js
@@ -3,14 +3,14 @@ function calculateHours() {
 	var employeeForm = document.forms['employeeform'];
 
 	//Get references to the elements of the form.
-	var employeeName = employeeForm.elements['employeename'];
-	var hourlyWage = employeeForm.elements['hourlywage'];
-	var weekDates = employeeForm.elements['weekDates'];
-	var mondayHours = employeeForm.elements['mondayhours'];
-	var tuesdayHours = employeeForm.elements['tuesdayhours'];
-	var wednesdayHours = employeeForm.elements['wednesdayHours'];
-	var thursdayHours = employeeForm.elements['thursdayhours'];
-	var fridayHours = employeeForm.elements['fridayhours'];
+	var employeeName = employeeForm.elements['employeename'].value;
+	var hourlyWage = employeeForm.elements['hourlywage'].value;
+	var weekDates = employeeForm.elements['weekDates'].value;
+	var mondayHours = employeeForm.elements['mondayhours'].value;
+	var tuesdayHours = employeeForm.elements['tuesdayhours'].value;
+	var wednesdayHours = employeeForm.elements['wednesdayHours'].value;
+	var thursdayHours = employeeForm.elements['thursdayhours'].value;
+	var fridayHours = employeeForm.elements['fridayhours'].value;
 
 	if(employeeName === "" || hourlyWage === "" || weekDates === "" || mondayHours === "" || tuesdayHours === "" || wednesdayHours === "" || thursdayHours === "" || fridayhours === "") {
 		window.alert("Make sure that all of the fields are filled in before you click 'Calculate'!");
