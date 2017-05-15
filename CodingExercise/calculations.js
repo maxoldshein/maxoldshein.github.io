5efed9bd0b16f10b922cb82c85a9dbbb48d7fc34
maxoldshein <oldshein@bc.edu>
2017-05-15 06:50:20 -0400

    Updated HTML/CSS/JavaScript for Athenium Challenge.

--- CodingExercise/calculations.js
+++ CodingExercise/calculations.js
@@ -60,26 +60,19 @@ function setCookie(cookieName, cookieValue, expirationTime) {
 	document.cookie = cookieName + "=" + cookieValue + ";" + expiration + ";path=/";
 }
 
-function getCookie(cookieName) {
-	console.log("!!IN GET COOKIE!!");
-	var name = cookieName + "=";
-	var cookieArray = document.cookie.split(';');
-	console.log(cookieArray);
-
-	for (var i = 0; i < cookieArray.length; i++) {
-		var c = cookieArray[i];
-
-		while (c.charAt(0) == ' ') {
-			c.substring(1);
-		}
-
-		if (c.indexOf(name) == 0) {
-			console.log(c.substring(name.length, c.length));
-			return c.substring(name.length, c.length);
-		}
-	}
-
-	return "";
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
 function checkEmployeeNameCookie() {
